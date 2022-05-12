97bca4142b1312e546689003d667b8206f4c0c3f
nitzanl <nitzan987654321@gmail.com>
2022-05-12 16:46:13 +0300

    lo meshane

---
+++ moodleGetFrom.py
@@ -0,0 +1,18 @@
+
+# returns list of (course_id, course_name)
+def get_course_list() -> list[(int, str)]:
+    return []
+
+
+# returns list of (resource_name, resource_type, resource_link, resource_course)
+def get_course_resources() -> list[(str, int, str)]:
+    RESTYPE_ASSIGNMENT = 1
+    RESTYPE_FORUM = 2
+    RESTYPE_DOCUMENT = 3
+
+    return []
+
+
+def get_resource_assignment(resource_link) -> dict:
+    # given assignment link, return dict of assignment info (due_date etc...)
+    return dict()
