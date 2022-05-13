ccc374c66a9e62cb13a749d930d666b7b4c7c191
nitzanl <nitzan987654321@gmail.com>
2022-05-13 04:17:45 +0300

    hopefully work imp

--- foraward_to_monday.py
+++ foraward_to_monday.py
@@ -8,6 +8,8 @@ from moodleGetFrom import get_course_list, get_course_resources, get_resource_as
 from APIKey import apiKey
 monday = MondayClient(apiKey)
 
+
+GROUP_NAME = 'Course Excercises'
 #our_board = 2663355974
 
 # returns list of (resource_name, resource_type, resource_link)
@@ -48,34 +50,6 @@ def get_item_id_by_name(board_id, group_id, item_name):
     return None
 
 
-#print(get_column_id_by_board_id_and_title(2666621719, 'Submission Date'))
-# print(monday.boards.fetch_boards())
-# print(monday.boards.fetch_boards_by_id())
-# print(get_item_id_by_name(2663355974, get_group_id_by_title(
-#     2663355974, 'Course Excercises'), "Excercise 1"))
-
-# id = get_item_id_by_name(2663355974, get_group_id_by_title(
-#     2663355974, 'Course Excercises'), "Excercise 1")
-# print(id)
-# print(monday.items.fetch_items_by_id([id]))
-
-
-def add_assignment_to_monday(board_id, assignments_list, assignment_name, group_assignment_id):
-    for a in assignments_list:
-        username = a[0]
-        a_course = a[1]
-        a_url = a[3]
-        a_name = a[4]
-        a_submit_date = a[5]
-        a_path_list = a[6]
-        monday.items.create_item(
-            board_id, group_assignment_id, assignment_name)
-        a_id = get_item_id_by_name(
-            board_id, group_assignment_id, assignment_name)
-    #monday.items.change_item_value(board, a_id, )
-    #print(monday.groups.get_items_by_group(our_board, 'matalot5384'))
-
-
 def add_item_to_group(boardID, groupID, item):
     monday.items.create_item(board_id=str(
         boardID), group_id=groupID,  item_name=item)
@@ -105,6 +79,89 @@ def add_person_to_task(itemID, boardID, colID):
     change_col_val(itemID, boardID, colID, person)
 
 
-change_col_val(2666502272, 2663355974, "date4", "2019-06-03 13:25:00")
+def create_link_json(url, text):
+    return f"{{\"url\":\"{url}\",\"text\":\"{text}\"}}"
 
-add_person_to_task(2666502272, 2663355974, "person")
+
+link = create_link_json("http://monday.com", "Go to Monday!")
+#print(get_column_id_by_board_id_and_title(2666621719, 'Submission Date'))
+# print(monday.boards.fetch_boards())
+# print(monday.boards.fetch_boards_by_id())
+# print(get_item_id_by_name(2663355974, get_group_id_by_title(
+#     2663355974, 'Course Excercises'), "Excercise 1"))
+
+# id = get_item_id_by_name(2663355974, get_group_id_by_title(
+#     2663355974, 'Course Excercises'), "Excercise 1")
+# print(id)
+# print(monday.items.fetch_items_by_id([id]))
+
+
+def add_assignments_to_monday(assignments_list):
+    for a in assignments_list:
+        username = a[0]
+        a_course = a[1]
+        a_url = a[3]
+        a_name = a[4]
+        a_submit_date = a[5]
+        a_path_list = a[6]
+        a_board_id = get_board_id_by_name(a_course)
+        a_group_id = get_group_id_by_title(a_board_id, GROUP_NAME)
+        # adding new assignment item to course board
+        monday.items.create_item(
+            a_board_id, a_group_id, a_name)
+        a_id = get_item_id_by_name(
+            a_board_id, a_group_id, a_name)
+        # add submission date
+        submit_date_col_id = get_column_id_by_board_id_and_title(
+            a_board_id, 'Submission Date')
+        change_col_val(a_id, a_board_id, submit_date_col_id, a_submit_date)
+        # add files
+        files_col_id = get_column_id_by_board_id_and_title(
+            a_board_id, 'Files')
+        for f in a_path_list:
+            monday.items.add_file_to_column(a_id, files_col_id, f)
+        # add submission
+        submission_col_id = get_column_id_by_board_id_and_title(
+            a_board_id, 'Submission')
+        change_col_val(a_id, a_group_id, submission_col_id, a_url)
+
+
+# Test for נושאים ב
+ # adding new assignment item to course board
+a_board_id = 2666626328
+a_group_id = get_group_id_by_title(a_board_id, GROUP_NAME)
+a_name = 'aadsds'
+a_submit_date = '2000-12-04'
+monday.items.create_item(
+    a_board_id, a_group_id, a_name)
+a_id = get_item_id_by_name(
+    a_board_id, a_group_id, a_name)
+# add submission date
+submit_date_col_id = get_column_id_by_board_id_and_title(
+    a_board_id, 'Submission Date')
+change_col_val(a_id, a_board_id, submit_date_col_id, a_submit_date)
+# add files
+# files_col_id = get_column_id_by_board_id_and_title(
+#     a_board_id, 'Files')
+# for f in a_path_list:
+#     monday.items.add_file_to_column(a_id, files_col_id, f)
+# add submission
+submission_col_id = get_column_id_by_board_id_and_title(
+    a_board_id, 'Submission')
+print(change_col_val(a_id, a_group_id, submission_col_id, link))
+
+# group_id = get_group_id_by_title(2666626328, 'Course Excercises')
+# item_id = get_item_id_by_name(2666626328, group_id, 'EX222')
+# files_col_id = get_column_id_by_board_id_and_title(
+#     2666626328, 'Files')
+# monday.items.add_file_to_column(item_id, files_col_id)
+
+#print(get_column_id_by_board_id_and_title(2666626328, 'Submission Date'))
+#monday.items.add_file_to_column(2666626692, files_col_id, f)
+#monday.items.change_item_value(board, a_id, )
+#print(monday.groups.get_items_by_group(our_board, 'matalot5384'))
+
+
+# change_col_val(2666502272, 2663355974, "date4", "2019-06-03 13:25:00")
+
+# add_person_to_task(2666502272, 2663355974, "person")
