b86befc1408e34fdbf655f90056debf818325932
Alon Harell <999alon@gmail.com>
2022-05-13 07:44:07 +0300

    really final

--- foraward_to_monday.py
+++ foraward_to_monday.py
@@ -10,8 +10,9 @@ monday = MondayClient(apiKey)
 
 GROUP_NAME = 'Course Excercises'
 #our_board = 2663355974
+maps = {"368217098":2666621719, "368306202":2666624447, "368317501":2666626328}
 
-# returns list of (resource_name, resource_type, resource_link)
+
 
 
 def get_board_id_by_name(board_name):
@@ -86,16 +87,7 @@ def create_link_json(url, text):
 
 
 link = create_link_json("http://monday.com", "Go to Monday!")
-#print(get_column_id_by_board_id_and_title(2666621719, 'Submission Date'))
-# print(monday.boards.fetch_boards())
-# print(monday.boards.fetch_boards_by_id())
-# print(get_item_id_by_name(2663355974, get_group_id_by_title(
-#     2663355974, 'Course Excercises'), "Excercise 1"))
 
-# id = get_item_id_by_name(2663355974, get_group_id_by_title(
-#     2663355974, 'Course Excercises'), "Excercise 1")
-# print(id)
-# print(monday.items.fetch_items_by_id([id]))
 
 
 def add_assignments_to_monday(assignments_list):
@@ -107,67 +99,25 @@ def add_assignments_to_monday(assignments_list):
         a_name = a[4]
         a_submit_date = a[5]
         a_path_list = a[6]
-        a_board_id = 2666624447  # get_board_id_by_name(a_course_id)
+        a_board_id = maps.get(a_course_id,0)  # get_board_id_by_name(a_course_id)
+        print(a_board_id)
+        if (a_board_id == 0):
+            continue
         a_group_id = get_group_id_by_title(a_board_id, GROUP_NAME)
         # adding new assignment item to course board
         monday.items.create_item(
             a_board_id, a_group_id, a_name)
         a_id = get_item_id_by_name(
             a_board_id, a_group_id, a_name)
-        print(a_id)
         # add submission date
         submit_date_col_id = get_column_id_by_board_id_and_title(
             a_board_id, 'Submission Date')
-        print(a_submit_date)
-        print(submit_date_col_id)
+
         change_col_val(a_id, a_board_id, submit_date_col_id, a_submit_date)
-        # add files
-        # files_col_id = get_column_id_by_board_id_and_title(
-        #     a_board_id, 'Files')
-        # for f in a_path_list:
-        #     monday.items.add_file_to_column(a_id, files_col_id, f)
-        # add submission
+
         submission_col_id = get_column_id_by_board_id_and_title(
             a_board_id, 'Submission')
         change_col_val(a_id, a_group_id, submission_col_id, a_url)
 
 
-# Test for נושאים ב
- # adding new assignment item to course board
-# a_board_id = 2666626328
-# a_group_id = get_group_id_by_title(a_board_id, GROUP_NAME)
-# a_name = 'aadsds'
-# a_submit_date = '2000-12-04'
-# monday.items.create_item(
-#     a_board_id, a_group_id, a_name)
-# a_id = get_item_id_by_name(
-#     a_board_id, a_group_id, a_name)
-# # add submission date
-# submit_date_col_id = get_column_id_by_board_id_and_title(
-#     a_board_id, 'Submission Date')
-# change_col_val(a_id, a_board_id, submit_date_col_id, a_submit_date)
-# # add files
-# # files_col_id = get_column_id_by_board_id_and_title(
-# #     a_board_id, 'Files')
-# # for f in a_path_list:
-# #     monday.items.add_file_to_column(a_id, files_col_id, f)
-# # add submission
-# submission_col_id = get_column_id_by_board_id_and_title(
-#     a_board_id, 'Submission')
-# print(change_col_val(a_id, a_group_id, submission_col_id, link))
 
-# group_id = get_group_id_by_title(2666626328, 'Course Excercises')
-# item_id = get_item_id_by_name(2666626328, group_id, 'EX222')
-# files_col_id = get_column_id_by_board_id_and_title(
-#     2666626328, 'Files')
-# monday.items.add_file_to_column(item_id, files_col_id)
-
-#print(get_column_id_by_board_id_and_title(2666626328, 'Submission Date'))
-#monday.items.add_file_to_column(2666626692, files_col_id, f)
-#monday.items.change_item_value(board, a_id, )
-#print(monday.groups.get_items_by_group(our_board, 'matalot5384'))
-
-
-# change_col_val(2666502272, 2663355974, "date4", "2019-06-03 13:25:00")
-
-# add_person_to_task(2666502272, 2663355974, "person")
--- moodleExtraction.py
+++ moodleExtraction.py
@@ -151,3 +151,4 @@ def main(username,personalid,password):
 
 
     driver.quit()
+
